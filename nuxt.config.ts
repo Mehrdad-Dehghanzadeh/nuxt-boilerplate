76fce076821480124eafed6a5b7f9856a138e276
Mehrdad Dehghanzadeh <mehrdad.dehghanzadeh.dev@gmail.com>
2025-11-21 21:18:54 +0330

    feat: add custom styles

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
+import path from 'node:path'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  css: [path.join(__dirname, 'app/assets/styles/custom/index.scss')],
+  alias: {
+    '@constants': path.join(__dirname, 'app/assets/constants'),
+    '@type': path.join(__dirname, 'types'),
+    '@models': path.join(__dirname, 'types/models'),
+    '@assets': path.join(__dirname, 'app/assets'),
+    '@styles': path.join(__dirname, 'app/assets/styles/global')
+  }
 })
---
+++ types/files.ts
@@ -0,0 +1,6 @@
+export type TFiles = 'image' | 'video' | 'excel' | 'word' | 'pdf'
+
+export type FilesOptionType = {
+  extensions: string[]
+  icon?: string
+}
